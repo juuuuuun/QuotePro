264eb5dbe7572bcfb2ef4c0165ec7cba55ed3a14
Jun Oh <kyungjunjoh@gmail.com>
2019-02-20 12:34:35 -0800

    TableView prototype with segue to QuoteBuilder view controller connected

--- QuotePro/HomeScreenViewController.swift
+++ QuotePro/HomeScreenViewController.swift
@@ -8,23 +8,48 @@
 
 import UIKit
 
-class HomeScreenViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class HomeScreenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+  
+  // MARK: Properties
+  @IBOutlet weak var tableView: UITableView!
+  
+  
+  // MARK: ViewController Methods
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+  
+  // MARK: UITableView Data Source
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "quoteCell", for: indexPath) as? QuoteTableViewCell else {
+      fatalError("QuoteTableViewCell was not dequeued")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    return cell
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
